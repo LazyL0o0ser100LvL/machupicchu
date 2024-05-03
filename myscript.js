adc4745a61a7e56f453178fbe0f795d7479feba1
LazyL0o0ser100LvL <kristina061200@icloud.com>
2024-05-03 14:52:34 +0300

    cite

---
+++ myscript.js
@@ -0,0 +1,9 @@
+function toggle(element) {
+
+    if(element.classList.contains("active")) {
+        element.classList.remove("active");
+    }
+    else {
+        element.classList.add("active");
+    }
+}
